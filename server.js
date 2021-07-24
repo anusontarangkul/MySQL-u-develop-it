6e080acdad73623a024a0fad96edc604c4468e3b
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-07-23 18:41:22 -0700

    post candidate

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -64,34 +65,29 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
 });
 
-// db.all(`SELECT * FROM candidates`, (err, rows) => {
-//     console.log(rows);
-// })
+// Create a candidate
+app.post('/api/candidates', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+                VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
 
-// db.get(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(row);
-// });
-
-// db.run(`DELETE FROM candidates WHERE id = ?`, 1, function (err, result) {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result, this, this.changes);
-// });
-
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//                 VALUES (?,?,?,?)`;
-
-// const params = [1, 'Ronald', 'Firbank', 1];
-// db.run(sql, params, function (err, result) {
-//     if (err) {
-//         console.log(err)
-//     }
-//     console.log(result, this.lastID);
-// });
+    db.run(sql, params, function (err, result) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body,
+            id: this.lastID
+        })
+    })
+});
 
 app.use((req, res) => {
     res.status(404).end();
